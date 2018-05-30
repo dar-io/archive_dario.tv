e8e24faf070f298f533abac7ac9d7551bd80973a
Dario <dario.ramalho@bbc.co.uk>
2018-05-30 15:37:39 +0100

    Removed lottie animation

--- _assets/js/global/main.js
+++ _assets/js/global/main.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+//
+//   var animation = bodymovin.loadAnimation({
+//   container: document.getElementById('lottie'), // Required
+//   path: '/assets/lottie/angry_emoji.json', // Required
+//   renderer: 'svg/canvas/html', // Required
+//   loop: true, // Optional
+//   autoplay: true, // Optional
+//   name: "Hello World", // Name for future reference. Optional.
+//   })
+// });
 
-  var animation = bodymovin.loadAnimation({
-  container: document.getElementById('lottie'), // Required
-  path: '/assets/lottie/angry_emoji.json', // Required
-  renderer: 'svg/canvas/html', // Required
-  loop: true, // Optional
-  autoplay: true, // Optional
-  name: "Hello World", // Name for future reference. Optional.
-  })
-});
+// <div id="lottie"></div>
